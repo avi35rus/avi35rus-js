89a64b8d9cd54437e74edc0edad05778b9febfce
avi35rus <104028363+avi35rus@users.noreply.github.com>
2022-08-04 21:28:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const iconMenu = document.querySelector('.toggle-nav');
+
 if (iconMenu){
 	const menuBody = document.querySelector('.menu__body');
 	const fence = document.querySelector('.logo');
 	const pageOverlay = document.querySelector('.page-overlay');
 
 	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+		fence.classList.toggle('_lock');
+		pageOverlay.classList.toggle('_lock');
+	});
+
+	pageOverlay.addEventListener("click", function(e) {
 		document.body.classList.toggle('_lock');
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
