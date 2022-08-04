4ef4933775db61337b468883a0176e930af600ea
avi35rus <104028363+avi35rus@users.noreply.github.com>
2022-08-04 21:03:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const iconMenu = document.querySelector('.toggle-nav');
+if (iconMenu){
+	const menuBody = document.querySelector('.menu__body');
+	const fence = document.querySelector('.logo');
+	const pageOverlay = document.querySelector('.page-overlay');
+
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+		fence.classList.toggle('_lock');
+		pageOverlay.classList.toggle('_lock');
+	});
+}
+
+function ScrollUp(){
+	let t, s;
+	s = document.body.scrollTop||window.pageYOffset;
+	t = setInterval (function() {
+		if(s > 0) window.scroll(0, s -= 5);
+		else clearInterval(t)
+	}, 5);
+}
